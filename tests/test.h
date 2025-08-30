7d984d0e11789423d8570bdfbbf203821b328c02
nickscha <nick@scharnbeck.de>
2025-08-30 18:49:33 +0200

    [DEP]updated test.h

--- tests/test.h
+++ tests/test.h
@@ -114,15 +114,7 @@ TEST_API TEST_INLINE void test_print_int(int val)
 }
 
 #else
-
-#ifndef _UNISTD_H
-#ifdef __APPLE__
-extern int write(int fd, void *buf, unsigned long count);
-#else
-extern int write(int fd, void *buf, unsigned int count);
-#endif
-#define STDOUT_FILENO 1
-#endif /* _UNISTD_H */
+#include <unistd.h>
 
 #define COLOR_DEFAULT 0
 #define COLOR_BLUE 0
